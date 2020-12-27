d66fc2734809be4ae45d3d7de5701e91c378f5fc
tRyzoNeT <55611037+tRyzoNeT@users.noreply.github.com>
2020-12-26 21:08:09 -0500

    Update addsfvname.tcl

--- preedb0t/preedb0t.v1.1.FRENCH.LiNUX.EGGDROP.TCL-RyZEN/addsfvname.tcl
+++ preedb0t/preedb0t.v1.1.FRENCH.LiNUX.EGGDROP.TCL-RyZEN/addsfvname.tcl
@@ -3,7 +3,7 @@ proc ms:addsfvname { nick uhost hand chan arg } {
  set chan [string tolower $chan]
  if {[channel get $chan add]} {
   if { $arg == "" } {
-   putquick "privmsg $chan : \002\0033(\00314ADDSFVNAME\0033)\002\0037 Syntax * !addsfvname <nom.de.la.release> <nom_du.sfv>"
+   putquick "privmsg $chan \002\0033(\00314ADDSFVNAME\0033)\002\0037 Syntax * !addsfvname <nom.de.la.release> <nom_du.sfv>"
   } else {
    set splitz [split $arg " "]
    set addsfvname_(release) [lrange $splitz 0 0]
@@ -12,13 +12,13 @@ proc ms:addsfvname { nick uhost hand chan arg } {
    set addsfvname_(nsfv) [lrange $splitz 1 1]
    set addsfvname_(nsfv) [string trimleft $addsfvname_(nsfv) "\{"]
    set addsfvname_(nsfv) [string trimright $addsfvname_(nsfv) "\}"]
-   if { $addsfvname_(release) == "" } { putquick "privmsg $chan : \002\0033(\00314ADDSFVNAME\0033)\002\0037 Erreur * NOM de la RELEASE MANQUANT" } else { }
-   if { $addsfvname_(nsfv) == "" } { putquick "privmsg $chan : \002\0033(\00314ADDSFVNAME\0033)\002\0037 Erreur * NOM du SFV MANQUANT" } else { }
+   if { $addsfvname_(release) == "" } { putquick "privmsg $chan \002\0033(\00314ADDSFVNAME\0033)\002\0037 Erreur * NOM de la RELEASE MANQUANT" } else { }
+   if { $addsfvname_(nsfv) == "" } { putquick "privmsg $chan \002\0033(\00314ADDSFVNAME\0033)\002\0037 Erreur * NOM du SFV MANQUANT" } else { }
   }
  }
  set chan [string tolower $chan]                                       
  if {[channel get $chan add]} {
-  global mysql_ db_
+  global mysql_ db_ chan_
   set splitz [split $arg " "]
   set addsfvname_(release) [lrange $splitz 0 0]
   set addsfvname_(release) [string trimleft $addsfvname_(release) "\\\{"]
@@ -30,7 +30,7 @@ proc ms:addsfvname { nick uhost hand chan arg } {
   set numrel [mysqlsel $mysql_(handle) $mysql_(select)]
   if { $numrel == 0 } { } else {                       
    mysqlexec $mysql_(handle) "UPDATE $mysql_(table) SET $db_(sfvname)='$addsfvname_(nsfv)' WHERE $db_(rlsname)='$addsfvname_(release)'"
-   #putquick "privmsg $predchan : (ADDSFVNAME) $addsfvname_(release) - $addsfvname_(nsfv)"
+   #putquick "privmsg $chan_(pred) (ADDSFVNAME) $addsfvname_(release) - $addsfvname_(nsfv)"
   }
  }
 }
