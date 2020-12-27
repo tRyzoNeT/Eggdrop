9643b8516cbad6ac6a87342b07f2fa1eea896c61
tRyzoNeT <55611037+tRyzoNeT@users.noreply.github.com>
2020-12-26 21:14:49 -0500

    Update modunnuke.tcl

--- preedb0t/preedb0t.v1.1.FRENCH.LiNUX.EGGDROP.TCL-RyZEN/modunnuke.tcl
+++ preedb0t/preedb0t.v1.1.FRENCH.LiNUX.EGGDROP.TCL-RyZEN/modunnuke.tcl
@@ -3,7 +3,7 @@ proc ms:modunnuke { nick uhost hand chan arg } {
  set chan [string tolower $chan]
  if {[channel get $chan add]} {
   if { $arg == "" } {
-   putquick "privmsg $chan :  \002\0033(\00314MODUNNUKE\0033)\002\0037 Syntax * !modunnuke <nom.de.la.release> <modunnuke.raison> <modunnuke.net>"
+   putquick "privmsg $chan \002\0033(\00314MODUNNUKE\0033)\002\0037 Syntax * !modunnuke <nom.de.la.release> <modunnuke.raison> <modunnuke.net>"
   } else {
    set splitz [split $arg " "]
    set modunnuke_(release) [lrange $splitz 0 0]
@@ -15,14 +15,14 @@ proc ms:modunnuke { nick uhost hand chan arg } {
    set modunnuke_(munnet) [lrange $splitz 2 2]
    set modunnuke_(munnet) [string trimleft $modunnuke_(munnet) "\{"]
    set modunnuke_(munnet) [string trimright $modunnuke_(munnet) "\}"]
-   if { $modunnuke_(release) == "" } { putquick "privmsg $chan : \002\0033(\00314MODUNNUKE\0033)\002\0037 Erreur * NOM de la RELEASE MANQUANT" } else { }
-   if { $modunnuke_(munraison) == "" } { putquick "privmsg $chan : \002\0033(\00314MODUNNUKE\0033)\002\0037 Erreur * RAiSON du modunnuke MANQUANT" } else { }
-   if { $modunnuke_(munnet) == "" } { putquick "privmsg $chan : \002\0033(\00314MODUNNUKE\0033)\002\0037 Erreur * NUKENET du modunnuke MANQUANT" } else { }
+   if { $modunnuke_(release) == "" } { putquick "privmsg $chan \002\0033(\00314MODUNNUKE\0033)\002\0037 Erreur * NOM de la RELEASE MANQUANT" } else { }
+   if { $modunnuke_(munraison) == "" } { putquick "privmsg $chan \002\0033(\00314MODUNNUKE\0033)\002\0037 Erreur * RAiSON du modunnuke MANQUANT" } else { }
+   if { $modunnuke_(munnet) == "" } { putquick "privmsg $chan \002\0033(\00314MODUNNUKE\0033)\002\0037 Erreur * NUKENET du modunnuke MANQUANT" } else { }
   }
  }
  set chan [string tolower $chan]
  if {[channel get $chan add]} {
-  global mysql_ db_
+  global mysql_ db_ chan_
   set splitz [split $arg " "]
   set modunnuke_(release) [lrange $splitz 0 0]
   set modunnuke_(release) [string trimleft $modunnuke_(release) "\\\{"]
@@ -38,7 +38,7 @@ proc ms:modunnuke { nick uhost hand chan arg } {
   set numrel [mysqlsel $mysql_(handle) $q]
   if { $numrel == 0 } { } else {                      
    mysqlexec $mysql_(handle) "UPDATE $mysql_(table) SET $db_(delpre)='0', $db_(moddelpre)='0', $db_(nuke)='0', $db_(modnuke)='0', $db_(unnuke)='0', $db_(undelpre)='0', $db_(modunnuke)='1', $db_(modunnuke_ctime)='$modunnuke_(munctime)', $db_(modunnuke_reason)='$modunnuke_(munraison)', $db_(modunnuke_net)='$modunnuke_(munnet)' WHERE $db_(rlsname)='$modunnuke_(release)'"
-   #putquick "privmsg $predchan : (MODUNNUKE) $modunnuke_(release) - $modunnuke_(munraison) / $modunnuke_(munnet)"
+   #putquick "privmsg $chan_(pred) (MODUNNUKE) $modunnuke_(release) - $modunnuke_(munraison) / $modunnuke_(munnet)"
   }
  }
 }
