2e4b48a71fcabe20feaf6f053c84438387733304
tRyzoNeT <55611037+tRyzoNeT@users.noreply.github.com>
2020-12-26 21:19:19 -0500

    Update unnuke.tcl

--- preedb0t/preedb0t.v1.1.FRENCH.LiNUX.EGGDROP.TCL-RyZEN/unnuke.tcl
+++ preedb0t/preedb0t.v1.1.FRENCH.LiNUX.EGGDROP.TCL-RyZEN/unnuke.tcl
@@ -3,7 +3,7 @@ proc ms:unnuke { nick uhost hand chan arg } {
  set chan [string tolower $chan]
  if {[channel get $chan add]} {
   if { $arg == "" } {
-   putquick "privmsg $chan :  \002\0033(\00314UNNUKE\0033)\002\0037 Syntax * !unnuke <nom.de.la.release> <unnuke.raison> <unnuke.net>"
+   putquick "privmsg $chan \002\0033(\00314UNNUKE\0033)\002\0037 Syntax * !unnuke <nom.de.la.release> <unnuke.raison> <unnuke.net>"
   } else {
    set splitz [split $arg " "]
    set unnuke_(release) [lrange $splitz 0 0]
@@ -15,14 +15,14 @@ proc ms:unnuke { nick uhost hand chan arg } {
    set unnuke_(unnet) [lrange $splitz 2 2]
    set unnuke_(unnet) [string trimleft $unnuke_(unnet) "\{"]
    set unnuke_(unnet) [string trimright $unnuke_(unnet) "\}"]
-   if { $unnuke_(release) == "" } { putquick "privmsg $chan : \002\0033(\00314UNNUKE\0033)\002\0037 Erreur * NOM de la RELEASE MANQUANT" } else { }
-   if { $unnuke_(unraison) == "" } { putquick "privmsg $chan : \002\0033(\00314UNNUKE\0033)\002\0037 Erreur * RAiSON du unnuke MANQUANT" } else { }
-   if { $unnuke_(unnet) == "" } { putquick "privmsg $chan : \002\0033(\00314UNNUKE\0033)\002\0037 Erreur * NUKENET du unnuke MANQUANT" } else { }
+   if { $unnuke_(release) == "" } { putquick "privmsg $chan \002\0033(\00314UNNUKE\0033)\002\0037 Erreur * NOM de la RELEASE MANQUANT" } else { }
+   if { $unnuke_(unraison) == "" } { putquick "privmsg $chan \002\0033(\00314UNNUKE\0033)\002\0037 Erreur * RAiSON du unnuke MANQUANT" } else { }
+   if { $unnuke_(unnet) == "" } { putquick "privmsg $chan \002\0033(\00314UNNUKE\0033)\002\0037 Erreur * NUKENET du unnuke MANQUANT" } else { }
   }
  }
  set chan [string tolower $chan]                                       
  if {[channel get $chan add]} {
-  global mysql_ db_
+  global mysql_ db_ chan_
   set splitz [split $arg " "]
   set unnuke_(release) [lrange $splitz 0 0]
   set unnuke_(release) [string trimleft $unnuke_(release) "\\\{"]
@@ -38,7 +38,7 @@ proc ms:unnuke { nick uhost hand chan arg } {
   set numrel [mysqlsel $mysql_(handle) $q]
   if { $numrel == 0 } { } else {                        
    mysqlexec $mysql_(handle) "UPDATE $mysql_(table) SET $db_(delpre)='0', $db_(moddelpre)='0', $db_(modnuke)='0', $db_(modunnuke)='0', $db_(nuke)='0', $db_(undelpre)='0', $db_(unnuke)='1', $db_(unnuke_ctime)='$unnuke_(unctime)', $db_(unnuke_reason)='$unnuke_(unraison)', $db_(unnuke_net)='$unnuke_(unnet)' WHERE $db_(rlsname)='$unnuke_(release)'"
-   #putquick "privmsg $predchan : (UNNUKE) $unnuke_(release) - $unnuke_(unraison) / $unnuke_(unnet)"
+   #putquick "privmsg $chan_(pred) (UNNUKE) $unnuke_(release) - $unnuke_(unraison) / $unnuke_(unnet)"
   }
  }
 }
