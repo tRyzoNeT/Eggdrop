8409b03cfda81f05b6d4c0c08344f09449fc92e0
tRyzoNeT <55611037+tRyzoNeT@users.noreply.github.com>
2020-12-28 16:01:53 -0500

    Update cmd.tcl

--- preedb0t/preedb0t.v1.1.FRENCH.LiNUX.EGGDROP.TCL-RyZEN/cmd.tcl
+++ preedb0t/preedb0t.v1.1.FRENCH.LiNUX.EGGDROP.TCL-RyZEN/cmd.tcl
@@ -7,7 +7,7 @@ proc ms:cmdlist { nick uhost hand chan arg } {
  putserv "privmsg $chan : + CMD disponible ::"
  if {[channel get $chan add]} {
   putserv "privmsg $chan - ADD :: !addpre !addurl !genre & !gn !info !readd"
-  putserv "privmsg $chan - ADD CHG :: !chgurl !chgsec
+  putserv "privmsg $chan - ADD CHG :: !chgurl !chgsec"
   putserv "privmsg $chan - ADD NUD :: !delpre !moddelpre !modnuke !modunnuke !nuke !undelpre !unnuke"
  }
  if {[channel get $chan db]} { putserv "privmsg $chan - DB iNFO :: !db" }
