7bec0bb08b0a5f9cf925157e9621a6de2663eac8
tRyzoNeT <55611037+tRyzoNeT@users.noreply.github.com>
2021-01-01 02:19:16 -0500

    Update cmd.tcl

--- preedb0t/preedb0t.v1.1.FRENCH.LiNUX.EGGDROP.TCL-RyZEN/cmd.tcl
+++ preedb0t/preedb0t.v1.1.FRENCH.LiNUX.EGGDROP.TCL-RyZEN/cmd.tcl
@@ -4,13 +4,14 @@
 bind pub -|- !cmd ms:cmdlist
 proc ms:cmdlist { nick uhost hand chan arg } {
  set chan [string tolower $chan]
- putserv "privmsg $chan : + CMD disponible ::"
+ putserv "privmsg $chan + CMD disponible ::"
  if {[channel get $chan add]} {
+  putserv "privmsg $chan - ADD :: !db_con (connect manuellement au mySQLd)"
   putserv "privmsg $chan - ADD :: !addpre !addurl !genre & !gn !info !readd"
-  putserv "privmsg $chan - ADD CHG :: !chgurl !chgsec"
-  putserv "privmsg $chan - ADD NUD :: !delpre !moddelpre !modnuke !modunnuke !nuke !undelpre !unnuke"
+  putserv "privmsg $chan - ADD :: !chgurl !chgsec"
+  putserv "privmsg $chan - ADD :: !delpre !moddelpre !modnuke !modunnuke !nuke !undelpre !unnuke"
  }
  if {[channel get $chan db]} { putserv "privmsg $chan - DB iNFO :: !db" }
- if {[channel get $chan search]} { putserv "privmsg $chan - SEARCH :: !dupe !pre" }
+ if {[channel get $chan search]} { putserv "privmsg $chan - RECHERCHE :: !dupe !pre" }
  if {[channel get $chan stats]} { putserv "privmsg $chan - STATS iNFO :: !hour" }
 }
