2dec7735825c8bdc345c9dc4b188f044f69252bf
ZarTeK Creole <ZarTeK.Creole@gmail.com>
2022-08-04 00:34:17 +0000

    indentation

--- preedb0t/preedb0t.v2.0.FRENCH.LiNUX.EGGDROP.TCL-RyZEN/connect.tcl
+++ preedb0t/preedb0t.v2.0.FRENCH.LiNUX.EGGDROP.TCL-RyZEN/connect.tcl
@@ -1,22 +1,22 @@
 bind pub -|- !db_con ms:dbconnect
 proc ms:dbconnect { nick uhost hand chan arg } {
- set chan [string tolower $chan]                                       
- if {[channel get $chan add]} {
-  global mysql_
-  set mysql_(handle) [mysqlconnect -host $mysql_(host) -user $mysql_(user) -password $mysql_(password) -db $mysql_(db)]
-  keepmysqlopen $mysql_(handle)
-  putquick "privmsg $chan \002\0033(\00314mySQLd\0033)\002\0037 Connexion au serveur mysqld avec success et KEEPMYSQLOPEN activer."
- }
+    set chan [string tolower $chan]
+    if {[channel get $chan add]} {
+        global mysql_
+        set mysql_(handle) [mysqlconnect -host $mysql_(host) -user $mysql_(user) -password $mysql_(password) -db $mysql_(db)]
+        keepmysqlopen $mysql_(handle)
+        putquick "privmsg $chan \002\0033(\00314mySQLd\0033)\002\0037 Connexion au serveur mysqld avec success et KEEPMYSQLOPEN activer."
+    }
 }
 
 proc keepmysqlopen {db_idle} {
- mysql::ping $db_idle
- after 2000 [list keepmysqlopen $db_idle]
+    mysql::ping $db_idle
+    after 2000 [list keepmysqlopen $db_idle]
 }
 
 proc evnt:init_server {type} {
- global mysql_
- set mysql_(handle) [mysqlconnect -host $mysql_(host) -user $mysql_(user) -password $mysql_(password) -db $mysql_(db)]
- keepmysqlopen $mysql_(handle)
- putlog "(mySQLd) Connexion au serveur mysqld avec success et KEEPMYSQLOPEN activer."
+    global mysql_
+    set mysql_(handle) [mysqlconnect -host $mysql_(host) -user $mysql_(user) -password $mysql_(password) -db $mysql_(db)]
+    keepmysqlopen $mysql_(handle)
+    putlog "(mySQLd) Connexion au serveur mysqld avec success et KEEPMYSQLOPEN activer."
 }
