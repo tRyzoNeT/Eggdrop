ca3beef644fbe0fe876079051e84212db473f4b7
ZarTeK Creole <ZarTeK.Creole@gmail.com>
2022-08-04 10:46:03 +0000

    Multi Changement structure

--- preedb0t/preedb0t.v2.0.FRENCH.LiNUX.EGGDROP.TCL-RyZEN/addpre.tcl
+++ preedb0t/preedb0t.v2.0.FRENCH.LiNUX.EGGDROP.TCL-RyZEN/addpre.tcl
@@ -1,48 +1,42 @@
-if { [catch { package require PREEDb0t 2.0 }] } { die "\[preed.tcl - erreur\] le fichier preed.tcl doit être charger en premier dans votre fichier eggdrop.conf" ; return }
+if { [catch { package require PREEDb0t-SQL 2.0 }] } { die "\[addpre.tcl - erreur\] le fichier connect.tcl doit être charger avant addpre.tcl" ; return }
 bind pub -|- !addpre ::PREEDb0t::addpre
 proc ::PREEDb0t::addpre { nick uhost hand chan arg } {
     set addpre_(time)       [clock seconds]
-    set chan                [string tolower $chan]
+    set chan                [string tolower ${chan}]
+    set addpre_(release)    [lindex ${arg} 0]
+    set addpre_(section)    [lindex ${arg} 1]
+    set addpre_(source)     [lindex ${arg} 2]
+    set addpre_(group)      [lindex [split ${addpre_(release)} -] end]
     if { ![channel get ${chan} p2add] } {
         putlog "L'utilisateur ${nick} à tenté un !addpre sur ${chan}, mais le salon n'a pas les *flags* necéssaire."
         return 0;
     }
-    if { ${arg} == "" } {
-        putquick "privmsg ${chan} \002\0033(\00314ADDPRE\0033)\002\0037 Syntax * !addpre <nom.de.la.release>"
-        return 0;
-    }
-    set addpre_(release)    [lindex ${arg} 0]
-    set addpre_(section)    [lindex ${arg} 1]
-    set addpre_(source)     [lindex ${arg} 2]
-    if { ${addpre_(release)} == "" } {
-        putquick "privmsg ${chan} \002\0033(\00314ADDPRE\0033)\002\0037 Erreur * NOM de la RELEASE MANQUANTE";
-        return 0;
-    }
-    if { ${addpre_(section)} == "" } {
-        putquick "privmsg ${chan} \002\0033(\00314ADDPRE\0033)\002\0037 Erreur * SECTiON MANQUANTE";
-        return 0;
-    }
+    # Note de ZarTek :
+    # si addpre_(source) est manquant, forcement ce n'ai pas bon, pas besoin de verifier addpre_(section), addpre_(section), addpre_(release)
+	# ATTENTION : dans nuke.tcl addpre(source) s'apelle nuke_(net),
+	# il est recommander de nommer tous source, ou tous net, mais pas une fois l'un puis l'autre pour respecter une logique
     if { ${addpre_(source)} == "" } {
-        putquick "privmsg ${chan} \002\0033(\00314ADDPRE\0033)\002\0037 Erreur * SOURCE MANQUANTE";
-        return 0
+		set IRC_MSG		[format "\002\0033(\00314ADDPRE\0033)\002\0037 Syntax * !addpre <nom.de.la.release> <section> <source>"]
+		putquick "privmsg ${chan} :${IRC_MSG}"
+        return 0;
     }
-    global network prefix_ mysql_ db_ chan_
-    set group               [lindex [split ${addpre_(release)} -] end]
+
     set SQL_INSERT          "INSERT IGNORE INTO ${::PREEDb0t::mysql_(table)} ( "
     append SQL_INSERT          "`${::PREEDb0t::db_(rlsname)}`, `${::PREEDb0t::db_(grp)}`, `${::PREEDb0t::db_(section)}`, "
     append SQL_INSERT          "`${::PREEDb0t::db_(ctime)}`, `${::PREEDb0t::db_(rls_addpre)}`, `${::PREEDb0t::db_(rls_addpre_nick)}`, "
     append SQL_INSERT          "`${::PREEDb0t::db_(rls_addpre_chan)}`, `${::PREEDb0t::db_(rls_addpre_network)}`, `${::PREEDb0t::db_(rls_addpre_source)}` "
     append SQL_INSERT       ") VALUES ( "
-    append SQL_INSERT           "'${addpre_(release)}', '${group}', '${addpre_(section)}', "
+    append SQL_INSERT           "'${addpre_(release)}', '${addpre_(group)}', '${addpre_(section)}', "
     append SQL_INSERT           "'${addpre_(time)}', '1', '${nick}', "
     append SQL_INSERT           "'${chan}', '${::network}', '${addpre_(source)}' "
     append SQL_INSERT       ");"
 
-    set SQL_RESULTAT [::mysql::exec ${mysql_(handle)} ${SQL_INSERT}];
+    set SQL_RESULTAT 	[::mysql::exec ${::PREEDb0t::mysql_(handle)} ${SQL_INSERT}];
     putlog "L'exécution de la requête a retourné: ${SQL_RESULTAT} pour ${addpre_(release)}"
-    if { [::mysql::insertid ${mysql_(handle)}] != "" } {
-        putlog "La release ${addpre_(release)} à été ajouter par ${chan}/${nick} (ID: [::mysql::insertid ${mysql_(handle)}])";
-        putquick "privmsg ${::PREEDb0t::chan_(pred)} \002\0033(\00314PRE\0033)\002\0037 (${addpre_(section)})\0030 ${addpre_(release)}"
+    if { [::mysql::insertid ${::PREEDb0t::mysql_(handle)}] != "" } {
+        putlog "La release ${addpre_(release)} à été ajouter par ${chan}/${nick} (ID: [::mysql::insertid ${::PREEDb0t::mysql_(handle)}])";
+		set IRC_MSG 	[format "\002\0033(\00314PRE\0033)\002\0037 (%s)\0030 %s" ${addpre_(section)} ${addpre_(release)}]
+        putquick "privmsg ${::PREEDb0t::chan_(pred)} :${IRC_MSG}"
     }
     return -1
 }
--- preedb0t/preedb0t.v2.0.FRENCH.LiNUX.EGGDROP.TCL-RyZEN/connect.tcl
+++ preedb0t/preedb0t.v2.0.FRENCH.LiNUX.EGGDROP.TCL-RyZEN/connect.tcl
@@ -1,23 +1,25 @@
 if { [catch { package require PREEDb0t 2.0 }] } { die "\[preed.tcl - erreur\] le fichier preed.tcl doit être charger en premier dans votre fichier eggdrop.conf" ; return }
-bind pub -|- !db_con ms:dbconnect
-proc ms:dbconnect { nick uhost hand chan arg } {
-    set chan [string tolower $chan]
-    if {[channel get $chan add]} {
-        global mysql_
-        set mysql_(handle) [mysqlconnect -host $mysql_(host) -user $mysql_(user) -password $mysql_(password) -db $mysql_(db)]
-        keepmysqlopen $mysql_(handle)
-        putquick "privmsg $chan \002\0033(\00314mySQLd\0033)\002\0037 Connexion au serveur mysqld avec success et KEEPMYSQLOPEN activer."
+bind pub -|- !db_con ::PREEDb0t::dbconnect
+proc ::PREEDb0t::dbconnect { nick uhost hand chan arg } {
+    set chan        [string tolower ${chan}]
+    if { [channel get ${chan} add] } {
+        set mysql_(handle)  [mysqlconnect -host ${::PREEDb0t::mysql_(host)} -user ${::PREEDb0t::mysql_(user)} -password ${::PREEDb0t::mysql_(password)} -db ${::PREEDb0t::mysql_(db)}]
+        keepmysqlopen ${mysql_(handle)}
+        set IRC_MSG     [format "\002\0033(\00314mySQLd\0033)\002\0037 Connexion au serveur mysqld avec success (ID: %s)et KEEPMYSQLOPEN activer." ${mysql_(handle)}]
+        putquick "privmsg ${chan} :${IRC_MSG}"
     }
 }
 
-proc keepmysqlopen {db_idle} {
-    mysql::ping $db_idle
-    after 2000 [list keepmysqlopen $db_idle]
+proc ::PREEDb0t::keepmysqlopen { mysql_(handle) } {
+    mysql::ping ${mysql_(handle)}
+    # Note de ZarTeK:
+    # Je pense que after est "bloquant" il est preferable d'utiliser timer ou utimer
+    after 2000 [list keepmysqlopen ${mysql_(handle)}]
 }
 
-proc evnt:init_server {type} {
-    global mysql_
-    set mysql_(handle) [mysqlconnect -host $mysql_(host) -user $mysql_(user) -password $mysql_(password) -db $mysql_(db)]
-    keepmysqlopen $mysql_(handle)
+proc evnt:init_server { } {
+    set mysql_(handle) [mysqlconnect -host ${::PREEDb0t::mysql_(host)} -user ${::PREEDb0t::mysql_(user)} -password ${::PREEDb0t::mysql_(password)} -db ${::PREEDb0t::mysql_(db)}]
+    ::PREEDb0t::keepmysqlopen ${mysql_(handle)}
     putlog "(mySQLd) Connexion au serveur mysqld avec success et KEEPMYSQLOPEN activer."
 }
+package provide PREEDb0t-SQL 2.0
