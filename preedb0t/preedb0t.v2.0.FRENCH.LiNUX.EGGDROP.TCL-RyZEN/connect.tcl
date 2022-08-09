f7b756bd90f0933f11c268c752b15c6d5d5edf1a
ZarTek Creole <zartek.creole@gmail.com>
2022-08-09 10:46:25 +0000

    bugfixe connexion sql

--- preedb0t/preedb0t.v2.0.FRENCH.LiNUX.EGGDROP.TCL-RyZEN/connect.tcl
+++ preedb0t/preedb0t.v2.0.FRENCH.LiNUX.EGGDROP.TCL-RyZEN/connect.tcl
@@ -3,23 +3,23 @@ bind pub -|- !db_con ::PREEDb0t::dbconnect
 proc ::PREEDb0t::dbconnect { nick uhost hand chan arg } {
     set chan        [string tolower ${chan}]
     if { [channel get ${chan} add] } {
-        set mysql_(handle)  [mysqlconnect -host ${::PREEDb0t::mysql_(host)} -user ${::PREEDb0t::mysql_(user)} -password ${::PREEDb0t::mysql_(password)} -db ${::PREEDb0t::mysql_(db)}]
-        keepmysqlopen ${mysql_(handle)}
-        set IRC_MSG     [format "\002\0033(\00314mySQLd\0033)\002\0037 Connexion au serveur mysqld avec success (ID: %s)et KEEPMYSQLOPEN activer." ${mysql_(handle)}]
+        set ::PREEDb0t::mysql_(handle)  [mysqlconnect -host ${::PREEDb0t::mysql_(host)} -user ${::PREEDb0t::mysql_(user)} -password ${::PREEDb0t::mysql_(password)} -db ${::PREEDb0t::mysql_(db)}]
+        keepmysqlopen ${::PREEDb0t::mysql_(handle)}
+        set IRC_MSG     [format "\002\0033(\00314mySQLd\0033)\002\0037 Connexion au serveur mysqld avec success (ID: %s)et KEEPMYSQLOPEN activer." ${::PREEDb0t::mysql_(handle)}]
         putquick "privmsg ${chan} :${IRC_MSG}"
     }
 }
 
-proc ::PREEDb0t::keepmysqlopen { mysql_(handle) } {
-    mysql::ping ${mysql_(handle)}
+proc ::PREEDb0t::keepmysqlopen {  } {
+    mysql::ping ${::PREEDb0t::mysql_(handle)}
     # Note de ZarTeK:
     # Je pense que after est "bloquant" il est preferable d'utiliser timer ou utimer
-    after 2000 [list keepmysqlopen ${mysql_(handle)}]
+    after 2000 [list keepmysqlopen ${::PREEDb0t::mysql_(handle)}]
 }
 
-proc evnt:init_server { } {
-    set mysql_(handle) [mysqlconnect -host ${::PREEDb0t::mysql_(host)} -user ${::PREEDb0t::mysql_(user)} -password ${::PREEDb0t::mysql_(password)} -db ${::PREEDb0t::mysql_(db)}]
-    ::PREEDb0t::keepmysqlopen ${mysql_(handle)}
+proc evnt:init_server { args } {
+    set ::PREEDb0t::mysql_(handle) [mysqlconnect -host ${::PREEDb0t::mysql_(host)} -user ${::PREEDb0t::mysql_(user)} -password ${::PREEDb0t::mysql_(password)} -db ${::PREEDb0t::mysql_(db)}]
+    ::PREEDb0t::keepmysqlopen ${::PREEDb0t::mysql_(handle)}
     putlog "(mySQLd) Connexion au serveur mysqld avec success et KEEPMYSQLOPEN activer."
 }
 package provide PREEDb0t-SQL 2.0
