a350e444883e907c5a7f9e8ca65d067ed777b7c3
tRyzoNeT <55611037+tRyzoNeT@users.noreply.github.com>
2022-09-16 23:15:23 -0400

    Update nuke.tcl

--- preedb0t/preedb0t.v2.0.FRENCH.LiNUX.EGGDROP.TCL-RyZEN/nuke.tcl
+++ preedb0t/preedb0t.v2.0.FRENCH.LiNUX.EGGDROP.TCL-RyZEN/nuke.tcl
@@ -5,40 +5,43 @@ proc ::PREEDb0t::nuke { nick uhost hand chan arg } {
 	set chan            [string tolower ${chan}]
 	set nuke_(release)  [lindex ${arg} 0]
 	set nuke_(raison)   [lindex ${arg} 1]
-	set nuke_(net)      [lindex ${arg} 2]
+	set nuke_(net)  [lindex ${arg} 2]
+	set nuke_(group)    [lindex [split ${nuke_(release)} -] end]
 	if { ![channel get ${chan} p2nuker] } {
 		putlog "L'utilisateur ${nick} à tenté un !nuke sur ${chan}, mais le salon n'a pas les *flags* nécessaire."
 		return 0;
 	}
-	# Note de ZarTek :
-	# si nuke_(net) est manquant, forcement ce n'ai pas bon, pas besoin de verifier addpre_(section), addpre_(section), addpre_(release)
-	# ATTENTION : dans addpre.tcl nuke_(net) s'apelle addpre(source),
-	# il est recommander de nommer tous source, ou tous net, mais pas une fois l'un puis l'autre pour respecter une logique
 	if { ${nuke_(net)} == "" } {
 		putquick "privmsg ${chan} \002\0033(\0034NUKE\0033)\002\0037 Syntax * !nuke <nom.de.la.release> <nuke.raison> <nukenet>"
 		return 0;
 	}
-	# Note de ZarTek :
-	# Si par exemple la rlsname est delpre, et ensuite nuke, le status delpre sera remis a 0 je ne suis pas sur que ce comportement est souhaité?
+    set SQL_INSERT          "INSERT IGNORE INTO ${::PREEDb0t::mysql_(tablenuke)} ( "
+    append SQL_INSERT          "`${::PREEDb0t::nuke_(rlsname)}`, `${::PREEDb0t::nuke_(grp)}`, "
+    append SQL_INSERT          "`${::PREEDb0t::nuke_(status)}`, `${::PREEDb0t::nuke_(ctime)}`, "
+    append SQL_INSERT          "`${::PREEDb0t::nuke_(reason)}`, `${::PREEDb0t::nuke_(nukenet)}` "
+    append SQL_INSERT       ") VALUES ( "
+    append SQL_INSERT           "'${nuke_(release)}', '${nuke_(group)}', "
+    append SQL_INSERT           "'nuke', '${nuke_(time)}', "
+    append SQL_INSERT           "'${nuke_(raison)}', '${nuke_(net)}' "
+    append SQL_INSERT       ");"
+    set SQL_RESULTAT 	[::mysql::exec ${::PREEDb0t::mysql_(handle)} ${SQL_INSERT}];
+	putlog "L'exécution de la requête a retourné: ${SQL_RESULTAT} pour ${nuke_(release)}"
+    if { [::mysql::insertid ${::PREEDb0t::mysql_(handle)}] != "" } { 
+		putlog "La release \002${nuke_(release)}\002 à été ajouter a la 'nukedb' par ${chan}/${nick} (ID: [::mysql::insertid ${::PREEDb0t::mysql_(handle)}])";
+	}
 	set SQL_UPDATE 		"UPDATE `${::PREEDb0t::mysql_(table)}` SET "
-	append SQL_UPDATE 		"`${::PREEDb0t::db_(delpre)}`			= '0' , "
-	append SQL_UPDATE 		"`${::PREEDb0t::db_(moddelpre)}`		= '0', "
-	append SQL_UPDATE 		"`${::PREEDb0t::db_(modnuke)}`			= '0', "
-	append SQL_UPDATE 		"`${::PREEDb0t::db_(modunnuke)}`		= '0', "
-	append SQL_UPDATE 		"`${::PREEDb0t::db_(unnuke)}`			= '0', "
-	append SQL_UPDATE 		"`${::PREEDb0t::db_(undelpre)}`			= '0', "
-	append SQL_UPDATE 		"`${::PREEDb0t::db_(nuke)}` 			= '1', "
+	append SQL_UPDATE 		"`${::PREEDb0t::db_(nuke_status)}`		= 'nuke', "
 	append SQL_UPDATE 		"`${::PREEDb0t::db_(nuke_ctime)}`		= '${nuke_(time)}', "
-	append SQL_UPDATE 		"`${::PREEDb0t::db_(nuke_reason)}` 		= '${nuke_(raison)}', "
-	append SQL_UPDATE 		"`${::PREEDb0t::db_(nuke_net)}` 		= '${nuke_(net)}', "
-	append SQL_UPDATE 		"(SELECT @update_id := id) "
-	append SQL_UPDATE	"WHERE `${::PREEDb0t::db_(rlsname)}` 		= '${nuke_(release)}';"
-	set SQL_ID			[::mysql::exec ${::PREEDb0t::mysql_(handle)} "SET @update_id := 0; ${SQL_UPDATE} SELECT @update_id;"];
+	append SQL_UPDATE 		"`${::PREEDb0t::db_(nuke_reason)}` 	= '${nuke_(raison)}', "
+	append SQL_UPDATE 		"`${::PREEDb0t::db_(nuke_net)}` 	= '${nuke_(net)}' "
+	append SQL_UPDATE	"WHERE `${::PREEDb0t::db_(rlsname)}` 	= '${nuke_(release)}'";
+	set SQL_ID			[::mysql::exec ${::PREEDb0t::mysql_(handle)} ${SQL_UPDATE}];
 	putlog "L'exécution de la requête a retourné: ${SQL_ID} pour ${nuke_(release)}"
 	if { ${SQL_ID} != "" } {
-		putlog "La release \002${nuke_(release)}\002 à été NUKE par ${chan}/${nick} (ID: ${SQL_ID})";
-		set IRC_MSG [format "\002\0033(\0034NUKE\0033)\002\0037 (%s - %s)\0030 %s" ${nuke_(raison)} ${nuke_(net)} ${nuke_(release)}]
+		putlog "La release \002${nuke_(release)}\002 à été NUKE par ${chan}/${nick} (ID: XxX)";
+		set IRC_MSG [format "\002\0033(\0034NUKE\0033)\002\0037 ${nuke_(raison)} ${nuke_(net)} ${nuke_(release)}"]
 		putquick "privmsg ${::PREEDb0t::chan_(nuke)} :${IRC_MSG}"
 	}
+	#
 	return -1
 }
